ff76e988fa3554b5cf4b77af8eb687dc6861cfcc
Kat,Jonathan J.Z <j.kat@student.fontys.nl>
2022-12-11 11:57:59 +0100

    begin leakage 2.0

--- Assets/Scripts/Leakage/LeakageManager.cs
+++ Assets/Scripts/Leakage/LeakageManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeakageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Scripts")]
+    [SerializeField] WaterManager waterManager;
+
+    [Header("Variables")]
+    public double autoWaterLeak;
+
+    
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Leak()
+    {
+
     }
 }
